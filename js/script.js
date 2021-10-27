db9445ee394f75bebdeb98487c2a2cea4d7fe21e
Mega-117 <enricosaian@hotmail.it>
2021-10-27 21:14:58 +0200

    modifiche js

--- js/script.js
+++ js/script.js
@@ -17,26 +17,26 @@ while (listaNumeriDaIndovinare.length <= 4) {
 console.log(listaNumeriDaIndovinare);
 
 //per visulizare tutti gli oggetti dell arr senza le virvole
-let textNumeriDaIndovinare = listaNumeriDaIndovinare.toString();
-let alertText = textNumeriDaIndovinare.replace(/\,/g, " ");
+//let alertText = virgolaRemove(listaNumeriDaIndovinare);
 
 
-alert("Ricoda questi numeri : " + alertText);
+
+alert("Ricoda questi numeri : " + listaNumeriDaIndovinare.join(" "));
 
 //countdown di 30 sec
 //dichiaro una variabile con i sec per il time
-let tempoTimer = 3;
+let tempoTimer = 30;
 let arrNumInseriti = [];
 let arrNumIndovinati = [];
 
 let interval = setInterval(function () {
 
     tempoTimer--;
-    console.log("tempoTimer " + tempoTimer);
+    console.log("Secondi " + tempoTimer);
     if (tempoTimer <= 0) {
         clearInterval(interval);
         while (arrNumInseriti.length <= 4) {
-            const boxNumInsetiti = parseInt(prompt("Ciao ti ricordi i 5 numeri scrivili separandoli con degli spazzi"));
+            const boxNumInsetiti = parseInt(prompt("Ciao ti ricordi i 5 numeri scrivine uno"));
             arrNumInseriti.push(boxNumInsetiti);
             //console.log("stringa num inseriti " + boxNumInsetiti);
             //console.log(arrNumInseriti);
@@ -54,13 +54,21 @@ let interval = setInterval(function () {
                 console.log(parseInt(arrNumIndovinati.length));
             }
         }
-        alert("hai indovinato " + arrNumIndovinati.length + " numeri su " + parseInt(listaNumeriDaIndovinare.length));
+        if (arrNumIndovinati.length === arrNumInseriti.length) {
+            alert("hai vinto");
+            alert("hai indovinato " + arrNumIndovinati.length + " numeri su " + parseInt(listaNumeriDaIndovinare.length));
+            alert("numeri indovinati: " + arrNumIndovinati.join(" ") + " Numeri dai indovinare: " + listaNumeriDaIndovinare.join(" "));
+
+        } else if (arrNumIndovinati.length < arrNumInseriti.length) {
+
+            alert("hai indovinato " + arrNumIndovinati.length + "<br> numeri su " + parseInt(listaNumeriDaIndovinare.length));
+            alert("numeri indovinati: " + arrNumIndovinati.join(" ") + " Numeri dai indovinare: " + listaNumeriDaIndovinare.join(" "));
+        }
 
     }
 
 
 }, 1000);
-console.log(arrNumIndovinati);
 
 
 
@@ -73,6 +81,12 @@ function numeriRandom() {
 
 //funz che splitta
 function stringSplit(parola) {
-    let wordSplit = parola.split(" ");
+    let wordSplit = parola.split(",");
     return wordSplit;
+}
+//per visulizare tutti gli oggetti dell arr senza le virvole in stringa
+function virgolaRemove(lista) {
+    let arrConVirgole = lista.toString();
+    let arrStinga = arrConVirgole.replace(/\,/g, " ");
+    return arrStinga;
 }
